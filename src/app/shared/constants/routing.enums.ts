590f61c4b946eca78260e0d710589dbafb03a9d6
Cyril <dck5132@gmail.com>
2025-08-13 16:29:09 -0400

    Updated enum values for internal pathing

--- src/app/shared/constants/routing.enums.ts
+++ src/app/shared/constants/routing.enums.ts
@@ -1,9 +1,14 @@
 export enum InternalPaths {
+    PAGE_TOP = 'page-top',
     HOME = 'home',
     HERO = 'hero',
     ABOUT = 'about',
     SKILLS = 'skills',
     RESUME = 'resume',
     PROJECTS = 'projects',
-    TESTIMONIALS = 'testimonials' 
+    TESTIMONIALS = 'testimonials',
+    CODE_SAMPLES = 'code-samples',
+    LOGIN_AUTH = 'login',
+    REST_API = 'rest_api',
+    WEBSOCKET = 'websocket'
 };
